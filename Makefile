bb7aef4b7f727f60001e640991eec6097027dc8a
Anton Popkov <dzerghinsky@gmail.com>
2020-11-22 14:35:59 +0300

    task_4

--- Makefile
+++ Makefile
@@ -8,5 +8,7 @@ package-install:
 	pip install --user dist/*.whl
 package-uninstall:
 	pip uninstall dist/*.whl
+lint:
+	poetry run flake8 brain_games
 
 .PHONY: install test lint selfcheck check build
--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,6 +1,13 @@
+"""Introduction module."""
+
 import prompt
 
+
 def welcome_user():
+    """
+    Greeting function
+    Return: user name
+    """
     name = prompt.string('Enter your name, please: ')
-    print(f'Hello, {name}!')
+    print('Hello, {}!'.format(name))
     return name
